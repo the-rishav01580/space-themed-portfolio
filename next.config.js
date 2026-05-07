450ca17c4b792f6680d24d1dcc0cf81dbd48bf7c
the-rishav01580 <rishavgarg01580@gmail.com>
2026-05-08 01:49:19 +0530

    vercel fix

--- next.config.js
+++ next.config.js
@@ -2,18 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   compress: true,
+
   images: {
     formats: ["image/webp"],
   },
+
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: "/:path*",
         headers: [
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "X-Frame-Options", value: "DENY" },
           { key: "X-XSS-Protection", value: "1; mode=block" },
-          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
         ],
       },
     ];
